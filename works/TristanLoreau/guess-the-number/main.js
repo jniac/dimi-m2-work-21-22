8fa26f5cf601d79a45c829db6d1c735a420f0a06
TristanLoreau <tristan.loreau@laposte.net>
2022-01-04 10:10:41 +0100

    js

---
+++ works/TristanLoreau/guess-the-number/main.js
@@ -0,0 +1,23 @@
+
+
+const numberTokenDiv = document.querySelector('div.number-token')
+numberTokenDiv.remove()
+
+const randomCloneNumberTokenDiv = (n) => {
+  const clone = numberTokenDiv.cloneNode(true)
+  document.body.append(clone)
+
+  const x = Math.floor(Math.random() * (window.innerWidth - 96))
+  const y = Math.floor(Math.random() * window.innerHeight)
+  clone.style.left = `${x}px`
+  clone.style.top = `${y}px`
+  clone.innerHTML = `#${n}`
+}
+
+const generateTokens = () => {
+  for (let i = 0; i < 100; i++) {
+    randomCloneNumberTokenDiv(i + 1)
+  }
+}
+
+generateTokens()
