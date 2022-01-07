9217ad1639f36c4afb5aa9c7da008d09b0ea2335
J-912-PC <identifier.j912@gmail.com>
2022-01-07 10:36:05 +0100

    up

--- works/j-912/guess-the-number/main.js
+++ works/j-912/guess-the-number/main.js
@@ -1,4 +1,4 @@
-/*const mysteryNumber = Math.round(Math.random() * 100)
+const mysteryNumber = Math.round(Math.random() * 100)
 
 const response = document.querySelector('div.response')
 response.remove()
@@ -50,26 +50,4 @@ document.body.onkeydown = (event) => {
   if (event.key === 'Enter') {
     submit()
   }
-}*/
-
-const numberTokenDiv = document.querySelector('div.number-token')
-numberTokenDiv.remove()
-
-const randomCloneNumberTokenDiv = (n) => {
-  const clone = numberTokenDiv.cloneNode(true)
-  document.body.append(clone)
-
-  const x = Math.floor(Math.random() * (window.innerWidth - 30))
-  const y = Math.floor(Math.random() * window.innerHeight)
-  clone.style.left = `${x}px`
-  clone.style.top = `${y}px`
-  clone.innerHTML = `#${n}`
 }
-
-const generateTokens = () => {
-  for (let i = 0; i < 100; i++) {
-    randomCloneNumberTokenDiv(i + 1)
-  }
-}
-
-generateTokens()
