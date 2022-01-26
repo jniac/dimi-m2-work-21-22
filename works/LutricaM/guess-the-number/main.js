1f100532b244254ad2d1a0b5dda305f965be8c12
LutriciaM <mvi.lutricia@gmail.com>
2022-01-26 20:52:25 +0100

    update

--- works/LutricaM/guess-the-number/main.js
+++ works/LutricaM/guess-the-number/main.js
@@ -1,14 +1,27 @@
-const mysteryNumber = Math.round(Math.random() * 100)
+const mysteryNumber = 8
 
-const response = document.querySelector('div.response')
-response.remove()
+/* const response = document.querySelector('div.response')
+ response.remove()
 
-const cloneResponse = (inputValue, commentValue) => {
-  const clone = response.cloneNode(true)
-  document.body.append(clone)
-  clone.querySelector('span.input').innerHTML = inputValue
-  clone.querySelector('span.comment').innerHTML = commentValue
-}
+ const cloneResponse = (inputValue, commentValue) => {
+   const clone = response.cloneNode(true)
+   document.body.append(clone)
+   clone.querySelector('span.input').innerHTML = inputValue
+   clone.querySelector('span.comment').innerHTML = commentValue
+ }*/
+
+const ballonImages = [
+  'assets/ballon-1.png',
+  'assets/BALLON-2.png',
+  'assets/BALLON-4.png',
+  'assets/BALLON-9.png',
+  'assets/BALLON-11.png',
+  'assets/tu-as-perdu.png',
+]
+let ballonImagesIndex = 0
+
+const avatar = document.querySelector('#avatar')
+const ballon = document.querySelector('#ballon')
 
 const submit = () => {
   const input = document.querySelector('input')
@@ -16,33 +29,45 @@ const submit = () => {
 
   document.body.classList.remove('wrong-state')
 
+  // on joue l'animation
+  avatar.classList.add('animated')
+  setTimeout(() => {
+    avatar.classList.remove('animated')
+  }, 2000)
+
+  // 
+  ballonImagesIndex = ballonImagesIndex + 1
+  const ballonImage = ballonImages[ballonImagesIndex]
+  ballon.style.backgroundImage = `url(${ballonImage})`
+
   if (isNaN(inputNumber)) {
 
-    cloneResponse(input.value, `This isn't a number`)
+    // cloneResponse(input.value, `This isn't a number`)
     document.body.classList.add('wrong-state')
 
   } else if (inputNumber < 0 || inputNumber > 100) {
 
-    cloneResponse(input.value, `Le nombre doit être compris entre 0 et 100.`)
+    // cloneResponse(input.value, `Le nombre doit être compris entre 0 et 100.`)
     document.body.classList.add('wrong-state')
 
   } else if (inputNumber < mysteryNumber) {
 
-    cloneResponse(input.value, `Too small.`)
+    cloneResponse(input.value, `perdu`)
 
   } else if (inputNumber > mysteryNumber) {
 
-    cloneResponse(input.value, `Too big.`)
+     cloneResponse(input.value, `perdu`)
 
   } else if (inputNumber === mysteryNumber) {
 
-    cloneResponse(input.value, `CONGRATS !`)
+     cloneResponse(input.value, `CONGRATS !`)
+     
   }
 
   input.value = ''
 }
 
-document.querySelector('button#submit').onclick = () => {
+document.querySelector('#submitButton').onclick = () => {
   submit()
 }
 
